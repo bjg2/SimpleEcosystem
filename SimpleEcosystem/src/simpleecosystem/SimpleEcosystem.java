05aa79b37e277186b77d370467baf62639706761
bjg2 <zaxa321@gmail.com>
2015-01-10 16:19:31 +0100

    Initial commit.

---
+++ SimpleEcosystem/src/simpleecosystem/SimpleEcosystem.java
@@ -0,0 +1,25 @@
+package simpleecosystem;
+
+import processing.core.PApplet;
+
+
+public class SimpleEcosystem extends PApplet {
+
+	private static final long serialVersionUID = 5665757813984678131L;
+
+	public void setup() {
+		size(displayWidth, displayHeight);
+	}
+
+	public void draw() {
+	}
+	
+	public boolean sketchFullScreen()
+	{
+		return true;
+	}
+	
+	public static void main(String _args[]) {
+		PApplet.main(new String[] { simpleecosystem.SimpleEcosystem.class.getName() });
+	}
+}
